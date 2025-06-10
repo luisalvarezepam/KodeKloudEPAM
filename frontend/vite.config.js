470318f5d819a974598114d59621745985faa2b6
Luis Alvarez <the_lg@hotmail.com>
2025-06-10 10:42:17 -0600

    adding ldap

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
-// https://vite.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'staticwebapp.config.json', // archivo a copiar
+          dest: '.' // lo coloca en la raíz de dist/
+        }
+      ]
+    })
+  ]
 });
