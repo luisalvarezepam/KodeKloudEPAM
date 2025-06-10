bccf30c25a043adf1576b919bf85bd4dbbf871bb
Luis Alvarez <the_lg@hotmail.com>
2025-06-10 13:46:55 -0600

    adding ldap

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,21 @@
+import { useEffect } from 'react';
 import KodeKloudDashboard from './KodeKloudDashboard';
 import './index.css';
 
 export default function App() {
+  useEffect(() => {
+    fetch("/.auth/me")
+      .then((res) => res.json())
+      .then((data) => {
+        if (!data.clientPrincipal) {
+          window.location.href = "/.auth/login/aad";
+        }
+      })
+      .catch((err) => {
+        console.error("Error verifying authentication:", err);
+        window.location.href = "/.auth/login/aad";
+      });
+  }, []);
+
   return <KodeKloudDashboard />;
 }
